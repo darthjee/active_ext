e62d560e819b77b69fb5ab3016a9fcbd33b5215e
darthjee <darthjee@gmail.com>
2017-06-21 17:50:03 +0200

    Extract pluck_as_json specs

--- spec/lib/active_record/relation_spec.rb
+++ spec/lib/active_record/relation_spec.rb
@@ -1,37 +1,13 @@
 require 'spec_helper'
 
 describe ActiveRecord::Relation do
+  let(:subject) { Document.all }
+
   describe '#percentage' do
-    it_behaves_like 'a method that returns the percentage of objects found' do
-      let(:subject) { Document.all }
-    end
+    it_behaves_like 'a method that returns the percentage of objects found'
   end
 
   describe '#pluck_as_json' do
-    let(:json) { Document.all.pluck_as_json(:id, :status) }
-    let(:expected) do
-      [ { id: 1, status: 'error' }, { id: 2, status: 'success' } ]
-    end
-
-    before do
-      Document.delete_all
-      Document.create(id: 1, status: :error)
-      Document.create(id: 2, status: :success)
-    end
-
-    it 'returns an array of hashes' do
-      expect(json).to eq(expected)
-    end
-
-    context 'when no arguments are given' do
-      let(:keys) do
-        Document.all.pluck_as_json.first.keys
-      end
-      let(:expected) {%w(id status updated_at created_at active)}
-
-      it 'returns all keys' do
-        expect(keys).to match_array(expected)
-      end
-    end
+    it_behaves_like 'a method that works as pluck but returning the keys'
   end
 end
--- spec/support/shared_examples/active_relation.rb
+++ spec/support/shared_examples/active_relation.rb
@@ -49,3 +49,31 @@ shared_examples 'a method that returns the percentage of objects found' do
     end
   end
 end
+
+shared_examples 'a method that works as pluck but returning the keys' do
+  let(:json) { subject.pluck_as_json(:id, :status) }
+  let(:expected) do
+    [ { id: 1, status: 'error' }, { id: 2, status: 'success' } ]
+  end
+
+  before do
+    Document.delete_all
+    Document.create(id: 1, status: :error)
+    Document.create(id: 2, status: :success)
+  end
+
+  it 'returns an array of hashes' do
+    expect(json).to eq(expected)
+  end
+
+  context 'when no arguments are given' do
+    let(:keys) do
+      subject.pluck_as_json.first.keys
+    end
+    let(:expected) {%w(id status updated_at created_at active)}
+
+    it 'returns all keys' do
+      expect(keys).to match_array(expected)
+    end
+  end
+end
