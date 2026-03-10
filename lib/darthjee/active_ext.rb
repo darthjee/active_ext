5027530087b607b6f16b27e04b6136dfa408de2e
Fernando Favini <darthjee@gmail.com>
2026-03-10 20:25:01 -0300

    Improve docs

--- lib/darthjee/active_ext.rb
+++ lib/darthjee/active_ext.rb
@@ -5,6 +5,7 @@ require 'active_record/relation_ext'
 require 'active_record/base_ext'
 
 module Darthjee
+  # Namespace for the gem.
   module ActiveExt
     require 'darthjee/active_ext/version'
   end
