ac222abe72566c18a43d4350671fa0e06bbf0ad9
Fernando Favini <darthjee@gmail.com>
2026-03-10 18:42:34 -0300

    Fix Style/HashSyntax

--- spec/support/schema.rb
+++ spec/support/schema.rb
@@ -3,7 +3,7 @@
 ActiveRecord::Schema.define do
   self.verbose = false
 
-  create_table :documents, :force => true do |t|
+  create_table :documents, force: true do |t|
     t.string :status
     t.boolean :active, default: false
     t.timestamps null: true
